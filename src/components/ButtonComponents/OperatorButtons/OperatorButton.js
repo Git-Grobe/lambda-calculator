23cd26181f1ef7db0022916fb51bb26dd7a8932e
Scott Grobe <gitgrobe@gmail.com>
2019-10-13 17:01:38 -0400

    basic component displays

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const OperatorButton = () => {
+export const OperatorButton = (props) => {
+  const { operators } = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {operators}
     </>
   );
 };
